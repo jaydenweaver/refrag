81eb041686db28807951917f4ad6e0d73a647b6e
Jayden <jayden_weaver@hotmail.com>
2026-05-08 16:15:32 +1000

    chore: update example page with img

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -4,24 +4,6 @@ import { useEffect, useRef } from "react";
 import { HtmlShader, type HtmlShaderHandle } from "refrag";
 import crtFrag from "./shaders/crt.frag?raw";
 
-const LOREM = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-  "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est qui dolorem ipsum quia dolor sit.",
-  "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.",
-  "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus itaque earum rerum hic tenetur.",
-  "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus.",
-  "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Neque porro quisquam est qui dolorem.",
-  "Ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur voluptatem.",
-  "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis molestie dictum semper, enim erat molestie pede, bibendum erat turpis eu lorem. Integer in mauris eu nibh euismod gravida.",
-  "Phasellus porttitor, metus non tincidunt dapibus, orci pede pretium neque, sit amet adipiscing ipsum lectus et libero. Aenean bibendum. Curabitur mattis quam id urna. Vivamus dui ligula, varius ut, euismod.",
-  "Donec aliquet, tortor sed accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et nisl hendrerit mollis. Vestibulum ut nisl mollie nam tempus lorem augue.",
-  "Fusce fermentum. Nullam varius nulla. Nunc volutpat convallis mauris. Proin eu sem. Nunc placerat enim vel risus. Sed a ipsum. Nunc aliquam, erat porttitor placerat tincidunt, mi orci iaculis lorem.",
-  "Viverra dictum leo vel tortor. Ut a elit sed dolor accumsan blandit et gravida mauris pharetra libero. Integer varius dui vel consequat porta nisl ligula congue neque at luctus ligula mauris pretium.",
-];
-
 const CONTENT_STYLE: React.CSSProperties = {
   color: "#ffffff",
   display: "flex",
@@ -30,6 +12,13 @@ const CONTENT_STYLE: React.CSSProperties = {
   padding: "4rem 1rem",
   gap: "2rem",
   textAlign: "center",
+};
+
+const textStyle: React.CSSProperties = {
+  fontFamily: "Georgia, 'Times New Roman', Times, serif",
+  fontSize: "clamp(1rem, 1.4vw, 1.25rem)",
+  lineHeight: 1.6,
+  maxWidth: "80ch",
 };
 
 function Content() {
@@ -46,18 +35,16 @@ function Content() {
       >
         text
       </h1>
-      {LOREM.map((p, i) => (
-        <p
-          key={i}
-          style={{
-            fontSize: "clamp(1rem, 1.4vw, 1.25rem)",
-            lineHeight: 1.6,
-            maxWidth: "70ch",
-          }}
-        >
-          {p}
-        </p>
-      ))}
+      <div style={{...textStyle}}>
+        this is a demo of the HtmlShader component, which allows you to apply a shader effect to any HTML content.      
+      </div>
+      <div style={{...textStyle}}>
+        the HTML content on this page is using a CRT shader, which gives the content a retro, pixelated look. you can scroll down to see more content and the shader effect will be applied to all of it.
+      </div>
+      <div style={{...textStyle}}>
+          heres an image of a red panda
+      </div>
+      <img src="/images/red-panda-sq.jpg" style={{ maxWidth: "100%", height: "auto" }} />
     </>
   );
 }
@@ -93,7 +80,7 @@ export function App() {
         frag={crtFrag}
         style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%" }}
       >
-        <div style={{ width: "100%", height: "100%", overflow: "hidden", background: "#101924" }}>
+        <div style={{ width: "100%", height: "100%", overflow: "hidden", background: "#0c0c0c" }}>
           <div ref={innerRef} style={CONTENT_STYLE}>
             <Content />
           </div>
